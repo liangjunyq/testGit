0cb813e3b94ca208a96d063635546061d29ff254
javakc80_global <javakc80_global@qq.com>
2020-08-26 19:00:26 +0800

    123

--- src/main/java/com/javakc/test1/TestGit2.java
+++ src/main/java/com/javakc/test1/TestGit2.java
@@ -3,5 +3,8 @@ package com.javakc.test1;
 public class TestGit2 {
     public static void main(String[] args) {
         System.out.println("你好");
+        int i=10;
+        i++;
+        System.out.println(i);
     }
 }
